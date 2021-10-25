b7c9b73dd58c6577e1000b5d71d39b3b191d961a
YataNox <rladnwls0523@naver.com>
2021-10-25 17:34:24 +0900

    Write Variable, getter, setter

--- WEB08_MemberMGT_UseServlet/src/main/java/com/ezen/dto/MemberDto.java
+++ WEB08_MemberMGT_UseServlet/src/main/java/com/ezen/dto/MemberDto.java
@@ -1,5 +1,49 @@
 package com.ezen.dto;
 
 public class MemberDto {
-
+	private String name;
+	private String userid;
+	private String userpwd;
+	private String email;
+	private String phone;
+	private String admin;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getUserpwd() {
+		return userpwd;
+	}
+	public void setUserpwd(String userpwd) {
+		this.userpwd = userpwd;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAdmin() {
+		return admin;
+	}
+	public void setAdmin(String admin) {
+		this.admin = admin;
+	}
+	
+	
 }
