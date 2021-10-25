8622fcfb94b31d325d689713be358a0df3e897ce
YataNox <rladnwls0523@naver.com>
2021-10-25 20:26:46 +0900

    Write doGetMethod

--- WEB08_MemberMGT_UseServlet/src/main/java/com/ezen/member/IdCheckServlet.java
+++ WEB08_MemberMGT_UseServlet/src/main/java/com/ezen/member/IdCheckServlet.java
@@ -1,11 +1,15 @@
 package com.ezen.member;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.ezen.dao.MemberDao;
 
 /**
  * Servlet implementation class IdCheckServlet
@@ -27,6 +31,15 @@ public class IdCheckServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
+		
+		String userid = request.getParameter("userid");
+		MemberDao mdao = MemberDao.getInstance();
+		// 전달된 아이디로 조회해서 조회결과(사용가능(-1)(or 사용중(1)))를 정수로 리턴받습니다.
+		int result = mdao.confirmID(userid);
+		request.setAttribute("result", result); // 검사 결과 request에 저장
+		request.setAttribute("userid", userid); // 아이디 request에 저장
+		RequestDispatcher dp = request.getRequestDispatcher("member/idcheck.jsp");
+		dp.forward(request, response);
 	}
 
 	/**
