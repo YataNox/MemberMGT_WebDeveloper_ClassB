26a22b1ad3d037a6c83d3e12fa4656da9a13ac7b
YataNox <rladnwls0523@naver.com>
2021-10-25 17:08:49 +0900

    Write checkSession part

--- WEB08_MemberMGT_UseServlet/src/main/java/com/ezen/member/LoginServlet.java
+++ WEB08_MemberMGT_UseServlet/src/main/java/com/ezen/member/LoginServlet.java
@@ -1,11 +1,14 @@
 package com.ezen.member;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class LoginServlet
@@ -26,16 +29,31 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setCharacterEncoding("UTF-8");
+		// 로그인 할 수 있는 화면으로 포워딩
+		String url = "member/loginForm.jsp"; // 포워딩 될 페이지의 경로 설정
+		
+		// 서블릿에서 세션은 아래와 같이 request에서 getSession()으로 전달받아야 사용이 가능합니다.
+		HttpSession session = request.getSession();
+		// .jsp 파일에는 이미 request와 response, session, application등이
+		// 이미 존재하기 때문에 session을 바로 사용하는 것이 가능하지만,
+		// 서블릿은 그렇지 않고, request와 response를 전달받아 사용하기 때문에
+		// 전달된 request를 통해 session을 꺼내서 사용합니다.
+		
+		// loginUesr 세션 값이 null이 아니라면
+		if(session.getAttribute("loginUser") != null) {
+			url = "main.jsp"; // 누군가 로그인 되어있는 상태라면 포워딩될 경로를 변경
+		}
+		
+		RequestDispatcher dp = request.getRequestDispatcher(url);
+		dp.forward(request, response); // 설정된 경로로 포워딩합니다.
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		request.setCharacterEncoding("UTF-8");
 	}
 
 }
